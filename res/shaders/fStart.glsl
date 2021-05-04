26e06edea3259993d3c6cfff768354c6f6eeb0cb
benha2941 <62339617+benha2941@users.noreply.github.com>
2021-05-04 20:54:57 +0800

    Update fStart.glsl
    task 2

--- res/shaders/fStart.glsl
+++ res/shaders/fStart.glsl
@@ -3,7 +3,9 @@ varying vec2 texCoord;  // The third coordinate is always 0.0 and is discarded
 
 uniform sampler2D texture;
 
+uniform float texScale;
+
 void main()
 {
-    gl_FragColor = color * texture2D( texture, texCoord * 2.0 );
+    gl_FragColor = color * texture2D( texture, texCoord * 2.0 * texScale);	// Where does the * 2 come from?
 }
