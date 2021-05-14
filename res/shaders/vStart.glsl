1de6c6fe0d06cd555a332131999ef683419dbb13
benha2941 <62339617+benha2941@users.noreply.github.com>
2021-05-14 22:45:12 +0800

    Update vStart.glsl

--- res/shaders/vStart.glsl
+++ res/shaders/vStart.glsl
@@ -21,6 +21,10 @@ void main()
 
     // The vector to the light from the vertex    
     vec3 Lvec = LightPosition.xyz - pos;
+    
+   	float lightDistance = length(Lvec);
+	
+	float attenuation = 1.0 / ( 1.0 + 1.0*lightDistance + pow(lightDistance,2.0) );
 
     // Unit direction vectors for Blinn-Phong shading calculation
     vec3 L = normalize( Lvec );   // Direction to the light source
@@ -46,7 +50,7 @@ void main()
 
     // globalAmbient is independent of distance from the light source
     vec3 globalAmbient = vec3(0.1, 0.1, 0.1);
-    color.rgb = globalAmbient  + ambient + diffuse + specular;
+    color.rgb = globalAmbient  + ambient + attenuation*diffuse + attenuation*specular;
     color.a = 1.0;
 
     gl_Position = Projection * ModelView * vpos;
