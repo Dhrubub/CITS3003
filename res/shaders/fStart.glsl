3dffe28eac5f7cb0c96898e12cae69da15a9a430
benha2941 <62339617+benha2941@users.noreply.github.com>
2021-05-17 02:28:48 +0800

    Update fStart.glsl

--- res/shaders/fStart.glsl
+++ res/shaders/fStart.glsl
@@ -1,11 +1,54 @@
-varying vec4 color;
-varying vec2 texCoord;  // The third coordinate is always 0.0 and is discarded
+vec4 color;
+
+varying vec3 Position;
+varying vec3 Normal;
+varying vec2 texCoord;
+
+uniform vec3 AmbientProduct, DiffuseProduct, SpecularProduct;
+uniform mat4 ModelView;
+uniform mat4 Projection;
+uniform vec4 LightPosition;
+uniform float Shininess;
 
 uniform sampler2D texture;
-
 uniform float texScale;
 
 void main()
 {
-    gl_FragColor = color * texture2D( texture, texCoord * 2.0 * texScale);	// Where does the * 2 come from?
+	vec4 vpos = vec4(Position, 1.0);
+	
+	vec3 pos = (ModelView * vpos).xyz;
+	
+	vec3 Lvec = LightPosition.xyz - pos;
+	
+	float lightDistance = length(Lvec);
+	
+	float attenuation = 1.0 / (5.0 + 9.0 * lightDistance + 5.0 * pow(lightDistance,2.0));
+	
+	vec3 L = normalize(Lvec);
+	vec3 E = normalize(-pos);
+	vec3 H = normalize(L + E);
+
+	vec3 N = normalize((ModelView * vec4(Normal, 0.0)).xyz);
+	
+	vec3 ambient = AmbientProduct;
+	
+	float Kd = max(dot(L, N), 0.0);
+	
+	vec3 diffuse = Kd * DiffuseProduct;
+	
+	float Ks = pow(max(dot(N,H),0.0), Shininess);
+	vec3 specular = Ks * SpecularProduct;
+	
+	if (dot(L, N) < 0.0) {
+		specular = vec3(0.0,0.0,0.0);
+	}
+	
+	vec3 globalAmbient = vec3(0.1,0.1,0.1);
+	
+	color.rgb = globalAmbient + attenuation * ambient + attenuation * diffuse + attenuation * specular;
+	
+	color.a = 1.0;
+
+	gl_FragColor = color * texture2D(texture, texCoord * 2.0 * texScale);
 }
