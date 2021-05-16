1d700bf2a9ad077ac83655b6c08473f9ce055773
Dhruv <dhruvjobanputra8@gmail.com>
2021-05-16 17:09:23 +0800

    separated Fshaders and Gshaders

---
+++ res/Gshaders/fStart.glsl
@@ -0,0 +1,11 @@
+varying vec4 color;
+varying vec2 texCoord;  // The third coordinate is always 0.0 and is discarded
+
+uniform sampler2D texture;
+
+uniform float texScale;
+
+void main()
+{
+    gl_FragColor = color * texture2D( texture, texCoord * 2.0 * texScale );
+}
--- src/scene-start.cpp
+++ src/scene-start.cpp
@@ -310,7 +310,7 @@ void init(void) {
     CheckError(); // Allocate texture objects
 
     // Load shaders and use the resulting shader program
-    shaderProgram = InitShader("res/shaders/vStart.glsl", "res/shaders/fStart.glsl");
+    shaderProgram = InitShader("res/Fshaders/vStart.glsl", "res/Fshaders/fStart.glsl");
 
     glUseProgram(shaderProgram);
     CheckError();
